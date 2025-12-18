5d6163d494f8b59df19513b5a8255b0baf774cd0
simsieec025 <simsieec025@student.wethinkcode.co.za>
2025-12-19 00:01:54 +0200

    my simple thought process

---
+++ log_analyzer.py
@@ -0,0 +1,20 @@
+def analyzer_log(file_log , keywords):
+    number_of_line = 0
+    
+    keywords_count = {keyword : 0}
+    try:
+        with open(file_log , "r") as file:
+            for line in file:
+                number_of_line += 1
+                for keyword in keywords:
+                    if keyword in line:
+                        keywords_count[keyword] += 1
+    except "FileNotFoundError":
+        print("ERROR: file not found")
+        return 
+    for keyword, count in keywords_count.items():
+        print(f"'{keyword}' number of occurence: {count}")
+        
+    file_log = "app.log"
+    keyword_in_keywords = ["ERROR", "CRITICAL", "WARNING", "FAILED"]
+    analyzer_log(file_log,keyword_in_keywords)
